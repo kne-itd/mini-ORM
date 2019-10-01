ae2eb82b2157f347636999a02b6ada37591d48bc
kaj <kne@edu.sde.dk>
2019-10-01 09:29:04 +0200

    Bugfix

--- ORM/Model/Patient.cs
+++ ORM/Model/Patient.cs
@@ -91,29 +91,5 @@ namespace ORM.Model
             int newID = Insert(tableName, Values, keys);
             Console.WriteLine(newID);
         }
-
- /*       private int Zzz(string TableName, ArrayList values, List<string> keys)
-        {
-            string fieldnames = string.Join(",", keys);
-            string parameters = "@" + string.Join(",@", keys);
-
-            string query = "INSERT INTO " + TableName + " (" + fieldnames + ")  " +
-                "VALUES " +
-                "(" + parameters + ")";
-
-            SqlCommand cmd = new SqlCommand(query, myConn);
-            for (int i = 0; i < keys.Count; i++)
-            {
-                cmd.Parameters.AddWithValue("@" + keys[i], values[i]);
-            }
-
-            Console.WriteLine(query);
-            myConn.Open();
-            int modified = (int)cmd.ExecuteScalar();
-            
-            myConn.Close();
-            return modified;
-        }
-        */
     }
 }
